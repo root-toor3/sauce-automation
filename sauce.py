1154c157baedf133950d10eb47d3b3beaa1074ee
root-toor3 <74669862+root-toor3@users.noreply.github.com>
2021-03-17 12:19:01 +0200

    Update sauce.py

--- sauce.py
+++ sauce.py
@@ -1,15 +1,8 @@
 print('type your code :')
 import webbrowser
-
 code = input()
-loop = 'sauce'
 strURL = "nhentai.net/g/" + code
 def page() :
-    webbrowser.open_new_tab(strURL) ;
-    
-page()
-    while loop == 'sauce' :
-    print('type your code :')
-    code = input()
+    webbrowser.open_new_tab(strURL) ;  
 page()
 
